20eb46795c89301d09f56ac6312f798a62abebd1
Majer9lazer <sidorenkoegor1999@mail.ru>
2018-12-18 21:21:13 +0600

    Started home work 4

--- HomeWork_3/Solution/base.php
+++ HomeWork_3/Solution/base.php
@@ -5,3 +5,10 @@
  * Date: 18.12.2018
  * Time: 17:01
  */
+const USERNAME_DB = 'root';
+const DSN = 'mysql:dbname=testdb;host=127.0.0.1';
+const PASSWORD = '';
+function Db()
+{
+    return new PDO(DSN, USERNAME_DB, PASSWORD);
+}
--- HomeWork_3/Solution/index.php
+++ HomeWork_3/Solution/index.php
@@ -16,6 +16,7 @@
     <title> Title</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <? require_once "base.php" ?>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
