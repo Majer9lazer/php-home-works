d21ed9af1872c9cdeb678191eb67b50efdeba3c8
Egor <32261965+Majer9lazer@users.noreply.github.com>
2019-01-13 22:26:27 +0600

    powershell build

---
+++ test.ps1
@@ -0,0 +1,10 @@
+﻿#[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+#для установления корректной кодировки при выводе сообщений на русском
+$msBuildStatus=msbuild  UI.csproj 
+if($LASTEXITCODE -eq 0){
+	echo 'Билд прошел успешно';
+	
+}else{
+echo 'Произошла ошибка';
+$msBuildStatus;
+}
